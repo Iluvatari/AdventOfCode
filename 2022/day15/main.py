0a56d49ee7da5f10cb17bb242911189bd31f3646
blash04 <blash04@gmail.com>
2022-12-25 14:10:13 -0600

    Solve day 15 part 1

--- 2022/day15/main.py
+++ 2022/day15/main.py
@@ -9,65 +9,89 @@ class Sensor:
         self.closestBeacon = beaconCoords
         self.distance = getManhattanDistance(self.coords, self.closestBeacon)
 
+    def coversPoint(self, coordToCheck: tuple[int]) -> bool:
+        distance = getManhattanDistance(self.coords, coordToCheck)
+        return distance <= self.distance
+
+    def pointsCoveredInRow(self, rowToCheck: int) -> set[int]:
+        distanceToRow = abs(self.coords[0] - rowToCheck)
+        remainingDistance = self.distance - distanceToRow
+        minBound = self.coords[1] - remainingDistance
+        maxBound = self.coords[1] + remainingDistance
+        pointsCovered = set(range(minBound, maxBound + 1))
+        return pointsCovered
+
 def processInput(fileText: list[str]):
     sensors = []
     #beaconCoords = []
-    minX = 100000
-    minY = 100000
-    maxX = 0
-    maxY = 0
+    minCol = 100000
+    minRow = 100000
+    maxCol = 0
+    maxRow = 0
     for currLine in fileText:
-        sensorX = int(currLine.split('Sensor at x=')[1].split(',')[0])
-        sensorY = int(currLine.split('Sensor at x=')[1].split(', y=')[1].split(':')[0])
+        sensorCol = int(currLine.split('Sensor at x=')[1].split(',')[0])
+        sensorRow = int(currLine.split('Sensor at x=')[1].split(', y=')[1].split(':')[0])
 
-        beaconX = int(currLine.split('closest beacon is at x=')[1].split(',')[0])
-        beaconY = int(currLine.split('closest beacon is at x=')[1].split(', y=')[1].split(':')[0])
-        minX = min(minX, sensorX - abs(sensorX - beaconX))
-        maxX = max(maxX, sensorX + abs(sensorX - beaconX))
+        beaconCol = int(currLine.split('closest beacon is at x=')[1].split(',')[0])
+        beaconRow = int(currLine.split('closest beacon is at x=')[1].split(', y=')[1].split(':')[0])
+        minCol = min(minCol, sensorCol - abs(sensorCol - beaconCol))
+        maxCol = max(maxCol, sensorCol + abs(sensorCol - beaconCol))
 
-        minY = min(minY, sensorY - abs(sensorY - beaconY))
-        maxY = max(maxY, sensorY + abs(sensorY - beaconY))
-        sensors.append(Sensor((sensorY, sensorX), (beaconY, beaconX)))
-        #beaconCoords.append((beaconX, beaconY))
-    xBounds = (minX, maxX)
-    yBounds = (minY, maxY)
-    return sensors, xBounds, yBounds
+        minRow = min(minRow, sensorRow - abs(sensorRow - beaconRow))
+        maxRow = max(maxRow, sensorRow + abs(sensorRow - beaconRow))
+        sensors.append(Sensor((sensorRow, sensorCol), (beaconRow, beaconCol)))
+    colBounds = (minCol, maxCol)
+    rowBounds = (minRow, maxRow)
+    return sensors, colBounds, rowBounds
 
-def isCloserThanAnyBeacon(beaconCoords: tuple[int, int], sensors: list[Sensor]) -> bool:
-    isCloser = False
-    for currSensor in sensors:
-        if getManhattanDistance(currSensor.coords, beaconCoords) <= currSensor.distance:
-            isCloser = True
-            return isCloser 
-    return False
-
-def isABeacon(beaconCoords: tuple[int, int], sensors: list[Sensor]) -> bool:
+def isABeacon(sensors: list[Sensor], coordToCheck: tuple[int, int]) -> bool:
     isABeacon = False
     for currSensor in sensors:
-        if beaconCoords == currSensor.closestBeacon:
+        if coordToCheck == currSensor.closestBeacon:
             isABeacon = True
             return isABeacon
     return False
 
-def findNumberImpossibleBeacon(sensors: list[Sensor], rowIdx: tuple[int, int], xBounds: tuple[int, int]) -> int:
-    numPossible = 0
-    for currBeaconNum in range(xBounds[0], xBounds[1] + 1):
-        if isABeacon((rowIdx, currBeaconNum), sensors):
-            continue
-        for currSensor in sensors:
-            if (rowIdx, currBeaconNum) == (currSensor.coords): #We are at the sensod
-                continue
-            if isCloserThanAnyBeacon((rowIdx, currBeaconNum), sensors):
-                numPossible += 1
-                break
-    return numPossible
+def isASensor(sensors: list[Sensor], coordToCheck: tuple[int, int]) -> bool:
+    isASensor = False
+    for currSensor in sensors:
+        if coordToCheck == currSensor.coords:
+            isASensor = True
+            return isASensor
+    return False
+
+def checkIfImpossible(sensors: list[Sensor], coord: tuple[int, int]) -> bool:
+    for sensor in sensors:
+        isCovered = sensor.coversPoint(coord)
+        if isCovered:
+            break
+    return isCovered
 
 fileH = open('input.txt', 'r')
 fileText = fileH.readlines()
-sensors, xBounds, yBounds = processInput(fileText)
-numPossibles = []
-for currRowNum in range(yBounds[0], yBounds[1] + 1):
-    numPossibles.append(findNumberImpossibleBeacon(sensors, currRowNum, xBounds))
-    print(numPossibles[-1])
-
-print(numPossibles[10 - yBounds[0]])
+sensors, colBounds, rowBounds = processInput(fileText)
+#ROW_NUM = 10
+ROW_NUM = 2000000
+#numImpossible = 0
+#for currX in range(xBounds[0], xBounds[1] + 1):
+#    if isABeacon(sensors, (ROW_NUM, currX)):
+#        continue
+#    if isASensor(sensors, (ROW_NUM, currX)):
+#        continue
+#    isImpossible = checkIfImpossible(sensors, (ROW_NUM, currX))
+#    if isImpossible:
+#        numImpossible += 1
+columns = set()
+for sensor in sensors:
+    pointsCovered = sensor.pointsCoveredInRow(ROW_NUM)
+    columns.update(pointsCovered)
+toRemove = []
+for column in columns:
+    if isABeacon(sensors, (ROW_NUM, column)):
+        toRemove.append(column)
+    if isASensor(sensors, (ROW_NUM, column)):
+        toRemove.append(column)
+for column in toRemove:
+    columns.remove(column)
+numImpossible = columns.__len__()
+print(numImpossible)   
